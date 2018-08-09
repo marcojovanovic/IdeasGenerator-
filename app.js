1e93e1086157eea9a4f5f71eb69dd7989cabaa86
Marco Jovanovic <klaudiusmarkus@gmail.com>
2018-08-09 17:57:42 +0200

    Updates

---
+++ app.js
@@ -0,0 +1,180 @@
+// Define UI Vars
+const form = document.querySelector('#task-form'),
+  taskList = document.querySelector('.list-group'),
+  clearBtn = document.querySelector('.clear-tasks'),
+  filter = document.querySelector('#filter'),
+  taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOm
+
+  document.addEventListener('DOMContentLoaded', getTasks)
+
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task
+  taskList.addEventListener('click', removeTask);
+
+  clearBtn.addEventListener('click', clearTasks);
+
+  filter.addEventListener('keyup', filterTasks);
+
+}
+function getTasks() {
+
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+
+  } else {
+
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+
+    li.className = 'list-group-item';
+
+    // text
+
+    li.appendChild(document.createTextNode(task));
+
+    // create new link element
+
+    const link = document.createElement('a');
+
+    link.className = 'delete-item ';
+
+    // icon html
+
+    link.innerHTML = '<i class="fas fa-trash"></i>';
+
+    li.appendChild(link);
+
+
+    taskList.appendChild(li);
+
+  })
+
+}
+// Add Task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li tag
+
+  const li = document.createElement('li');
+
+  li.className = 'list-group-item';
+
+  // text
+
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  // create new link element
+
+  const link = document.createElement('a');
+
+  link.className = 'delete-item ';
+
+  // icon html
+
+  link.innerHTML = '<i class="fas fa-trash"></i>';
+
+  li.appendChild(link);
+
+
+  taskList.appendChild(li);
+
+  // Store to localStorage
+
+  storeTaskInLocalStorage(taskInput.value)
+
+  // clear input
+  taskInput.value = '';
+  e.preventDefault();
+}
+function storeTaskInLocalStorage(task) {
+
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+
+  } else {
+
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+
+// remove taks
+
+function removeTask(e) {
+
+
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Da li ste sigurni?')) {
+
+      e.target.parentElement.parentElement.remove();
+    }
+
+    // remove from localstorage
+
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+
+  }
+}
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+// clear tasks
+
+function clearTasks() {
+
+  taskList.innerHTML = '';
+  // Clear from LS
+  clearTasksFromLocalStorage();
+}
+// Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+function filterTasks(e) {
+
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.list-group-item').forEach((task) => {
+    const item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
+}
